657c82b8ceaf8f873dc385053369b3ab21909f42
ValentinaRippe <lauravalentina200102@gmail.com>
2021-11-26 10:55:25 -0500

    Cambios CSS, JavaScript y HTML 1

--- script.js
+++ script.js
@@ -4,11 +4,40 @@ function vermas() {
     const htmlShow = document.getElementById("verlista");
   if (htmlShow.style.display === "none") {
     htmlShow.style.display = "block";
-  } else {
+  }
+   else {
     htmlShow.style.display = "none";
   }
-  const uno = document.getElementById('vermas');
-  if (uno.innerHTML == 'Ver lista') 
-      uno.innerHTML = 'Ver menos.';
-  else uno.innerHTML = 'Ver lista'; 
+   const cambiar = document.getElementById("vermas");
+  if (cambiar.innerHTML=="Ver lista")
+     cambiar.innerHTML = "Ver menos";
+  else cambiar.innerHTML= "Ver lista";
+} 
+
+function vermas2() {
+  const htmlShow = document.getElementById("VerCartasC");
+if (htmlShow.style.display === "none") {
+  htmlShow.style.display = "block";
 }
+ else {
+  htmlShow.style.display = "none";
+}
+ const cambiar = document.getElementById("vermas2");
+if (cambiar.innerHTML=="Ver Cartas")
+   cambiar.innerHTML = "Ver menos";
+else cambiar.innerHTML= "Ver Cartas";
+} 
+
+function vermas3() {
+  const htmlShow = document.getElementById("VerCartasS");
+if (htmlShow.style.display === "none") {
+  htmlShow.style.display = "block";
+}
+ else {
+  htmlShow.style.display = "none";
+}
+ const cambiar = document.getElementById("vermas3");
+if (cambiar.innerHTML=="Ver Cartas")
+   cambiar.innerHTML = "Ver menos";
+else cambiar.innerHTML= "Ver Cartas";
+} 
